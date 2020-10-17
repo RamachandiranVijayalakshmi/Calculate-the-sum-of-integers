dee51b04eb48baa16d7265dc0d0cc4d8c7be0c24
RamachandiranVijayalakshmi <66218468+RamachandiranVijayalakshmi@users.noreply.github.com>
2020-10-17 23:55:42 +0530

    Add files via upload

---
+++ Calculate the sum of integers.py
@@ -0,0 +1,7 @@
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return a+b
+num1 = int(input('Enter the a value'))
+num2 = int(input('Enter the b value'))
+res = solveMeFirst(num1,num2)
+print('adding value:',res)
